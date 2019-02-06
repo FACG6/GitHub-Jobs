1cabe8a74c992dbc126ced1b0ad34e80fd8f9ab1
Israa <israa96sulaiman@gmail.com>
2019-02-06 08:54:01 +0200

    complete HandleSearch and makeRequest functions

---
+++ src/makeRequest.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const makeRequest = (data, callback) => {
+    const description = data.split(',')[0];
+    const location = data.split(',')[1];
+    const option = {
+        url: `https://jobs.github.com/positions.json?description=${description}&location=${location}`,
+        method: 'GET',
+    }
+    request(option, (err, res) => {
+        if (res.statusCode !== 200) {
+            callback(`statusCode is ${res.statusCode}`);
+            return;
+        }
+        if (!res.headers['content-type'].includes('application/json')) {
+            callback(`Not jason response. Content type is ${res.headers['content-type']}`);
+            return;
+        }
+        callback(null, res.body);
+    })
+
+}
+
+module.exports = makeRequest;
+
+
